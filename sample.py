d0461086730bc013aead293c739559011934fe39
Farhan <muhammad.farhan@soco-engineers.com>
2024-07-10 14:27:38 +0500

    added Makefile

---
+++ sample.py
@@ -0,0 +1,10 @@
+"""These are some of the functions that we will be dealing for math operation.
+"""
+
+
+def add_data(a: float, b: float):
+    return a + b
+
+
+def subtract_data(a: float, b: float):
+    return a - b
